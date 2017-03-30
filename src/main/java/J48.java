e43aecee76e671be48e0fd02c47546a9eb1c5aa4
kai18 <kaustubh0163@gmail.com>
2017-03-30 20:36:34 +0530

    Added Stubs

---
+++ src/main/java/J48.java
@@ -0,0 +1,5 @@
+/**
+ * Created by kaustubh on 3/30/17.
+ */
+public class J48 {
+}
--- src/main/java/NeuralNetwork.java
+++ src/main/java/NeuralNetwork.java
@@ -2,4 +2,7 @@
  * Created by kaustubh on 3/28/17.
  */
 public interface NeuralNetwork {
+    void train(double input[]);
+    void recall(double input[]);
+    void train(double inputpp[], double output[]);
 }
