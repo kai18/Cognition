61bf7ab02ec2113437a4eab0d18558e09b178f59
kai18 <kaustubh0163@gmail.com>
2017-05-01 21:34:56 +0530

    Adding backpropagation implementation

--- src/test/java/GenericLayer.java
+++ src/test/java/GenericLayer.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+
 /**
  * Created by kaustubh on 5/1/17.
  */
-public class GenericLayer {
+public class GenericLayer{
+
+    int numNeurons;
+    ArrayList <Neuron> neurons;
+    String activationFunction= null;
+    String inputFunction = null;
+
+    public int getNumNeurons() {
+        return neurons.size();
+    }
+
+    public ArrayList<Neuron> getNeurons() {
+        return neurons;
+    }
+
+    public void setNeurons(double[] input) {
+        for (double in: input)
+        {
+            Neuron n = new InputNeuron();
+            n.setValue(in);
+            neurons.add(n);
+        }
+    }
+
+    public void setNeurons(ArrayList<Neuron> neurons) {
+        this.neurons = neurons;
+    }
+
+    public void addNeuron(Neuron neuron)
+    {
+        neurons.add(neuron);
+    }
 }
